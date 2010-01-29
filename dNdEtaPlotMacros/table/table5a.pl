3063654345edc745c5539359d2458b79bff203a6
yilmaz <>
2010-01-29 14:11:05 +0000

    fix

--- dNdEtaPlotMacros/table/table5a.pl
+++ dNdEtaPlotMacros/table/table5a.pl
@@ -10,10 +10,12 @@ chomp @valuesX;
 chomp @valuesY;
 chomp @errorsY;
 
+$xMax = 2.;
+
 print "pt  value  error \n";
 $counter = 0;
 $offset = 0;
-for($i = 1; $i < scalar @valuesX; $i++){
+for($i = 0; $i < scalar @valuesX; $i++){
     
     if($valuesX[$i] =~ /B/){
 	$counter++;
@@ -23,10 +25,15 @@ for($i = 1; $i < scalar @valuesX; $i++){
 	    $offset = $offset + 4;
 	}
     }
-    
+
+    if( $counter % 2 == 0){
+
+    if($valuesX[$i] < $xMax){
     $valueY = $valuesY[$i] - $offset;
     $valueY = `printf "%0.5f" $valueY`;
     print $valuesX[$i], " ",$valueY, "  ", $errorsY[$i], " \n"  ;    
+}
+}
 }
 
 
