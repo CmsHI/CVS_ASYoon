80b842ac214df30283bc3c6d5f93e5a5feac4e68
sungho <>
2010-05-03 16:17:12 +0000

    default changed

--- SpectraAna/TrackSpectraAnalyzer/python/trackspectraanalyzer_cfi.py
+++ SpectraAna/TrackSpectraAnalyzer/python/trackspectraanalyzer_cfi.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 trackAna = cms.EDAnalyzer('TrackSpectraAnalyzer',
-        src = cms.untracked.InputTag("generalTracks"),
+        src = cms.untracked.InputTag("selectTracks"),
         vsrc = cms.untracked.InputTag("offlinePrimaryVertices"),
-        jsrc = cms.untracked.InputTag("ak5CaloJets"),
+        jsrc = cms.untracked.InputTag("iterativeCone5CaloJets"),
         doOutput = cms.untracked.bool(True),
         isGEN = cms.untracked.bool(True),
         doJet = cms.untracked.bool(True),
