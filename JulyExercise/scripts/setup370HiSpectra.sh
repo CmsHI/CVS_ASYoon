e3e26401872fd2dc25c4550a30c40bb844ba1722
sungho <>
2010-07-07 17:15:25 +0000

    update

--- JulyExercise/scripts/setup370HiSpectra.sh
+++ JulyExercise/scripts/setup370HiSpectra.sh
@@ -7,7 +7,7 @@ cvs co UserCode/edwenger/Skims
 
 # analyzers
 cvs co UserCode/edwenger/VertexAnalyzer
-cvs co UserCode/edwenger/TrackSpectraAnalyzer
+cvs co UserCode/edwenger/HiTrackSpectraAnalyzer
 cvs co UserCode/edwenger/TrkEffAnalyzer
 
 # july ex. 
@@ -28,3 +28,5 @@ mv UserCode/edwenger .
 mv UserCode/SavedFMa/Saved/ .
 rm -r UserCode/ASYoon
 rm -r UserCode/SavedFMa
+
+scram b
