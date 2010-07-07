32dc7a83bdde36ed219cde20a85de525fddb5d2e
sungho <>
2010-07-07 14:10:30 +0000

    first checkin

---
+++ JulyExercise/scripts/setup370HiSpectra.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+cd $CMSSW_BASE/src
+
+# skiming code
+cvs co UserCode/edwenger/Skims
+
+# analyzers
+cvs co UserCode/edwenger/VertexAnalyzer
+cvs co UserCode/edwenger/TrackSpectraAnalyzer
+cvs co UserCode/edwenger/TrkEffAnalyzer
+
+# july ex. 
+cvs co UserCode/ASYoon/JulyExercise
+cvs co UserCode/SavedFMa/Saved
+
+# replace with hi-specific 
+rm -r UserCode/edwenger/Skims/python
+mv UserCode/ASYoon/JulyExercise/python UserCode/edwenger/Skims/
+cp UserCode/ASYoon/JulyExercise/test/*.* UserCode/edwenger/Skims/test
+
+# changes needed for 370
+cp UserCode/ASYoon/JulyExercise/interface/HackedAnalyticalTrackSelector.h UserCode/edwenger/TrkEffAnalyzer/interface
+cp UserCode/ASYoon/JulyExercise/src/SealModules.cc UserCode/edwenger/TrkEffAnalyzer/src
+
+# cleanup
+mv UserCode/edwenger .
+mv UserCode/SavedFMa/Saved/ .
+rm -r UserCode/ASYoon
+rm -r UserCode/SavedFMa
