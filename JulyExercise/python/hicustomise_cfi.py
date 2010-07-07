7a5cd2d334d5805f0306a6c64ab0f4fb21ebe971
sungho <>
2010-07-07 23:24:05 +0000

    to hitrackAna

--- JulyExercise/python/hicustomise_cfi.py
+++ JulyExercise/python/hicustomise_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 # this is to enable or disable features as necessary for running over MC
 def enableSIM(process):
-    process.trackAna.isGEN=True
+    process.hitrackAna.isGEN=True
     process.trkEffAnalyzer.hasSimInfo=True
     return process
 
