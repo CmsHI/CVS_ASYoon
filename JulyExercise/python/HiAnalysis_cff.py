d9804cd020dd128d52acf4ccad8cfc4ca2d30c13
sungho <>
2010-07-08 20:27:05 +0000

    mult option

--- JulyExercise/python/HiAnalysis_cff.py
+++ JulyExercise/python/HiAnalysis_cff.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 from edwenger.Skims.HiAnalyzers_cff import *
 
-hiAnalysisSeq = cms.Sequence(hitrackAna*
-                             trkEffAnalyzer)
+hiAnalysisSeq = cms.Sequence((hitrackAna + hitrackAnaMult)*
+                             (hitrkEffAnalyzer + hitrkEffAnalyzerMult))
