662f6499946e2a970e91b8fdeba8577919cddd4e
sungho <>
2010-07-08 22:59:35 +0000

    option for *Mult ana

--- JulyExercise/python/hicustomise_cfi.py
+++ JulyExercise/python/hicustomise_cfi.py
@@ -3,7 +3,9 @@ import FWCore.ParameterSet.Config as cms
 # this is to enable or disable features as necessary for running over MC
 def enableSIM(process):
     process.hitrackAna.isGEN=True
+    process.hitrackAnaMult.isGEN=True
     process.hitrkEffAnalyzer.hasSimInfo=True
+    process.hitrkEffAnalyzerMult.hasSimInfo=True
     return process
 
 
