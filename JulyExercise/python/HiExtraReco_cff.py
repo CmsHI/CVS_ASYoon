9bb95aa7fc91db93f87e70a09e81db459b3d7120
sungho <>
2010-07-10 21:49:10 +0000

    refit tracks with vertex constraint

--- JulyExercise/python/HiExtraReco_cff.py
+++ JulyExercise/python/HiExtraReco_cff.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 from edwenger.Skims.HiLowPtPxlRereco_cff import *
 from edwenger.Skims.HiCentralityInfo_cff import *
+from edwenger.Skims.HiTrackRefit_cff import *
 
 hiextraReco = cms.Sequence(hicentProd*
-                           hiLowPtPixelTracks)
+                           hiLowPtPixelTracks*
+                           hitrackRefit)
