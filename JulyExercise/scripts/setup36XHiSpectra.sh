4d30ae5dd51f78a7ef1fddcdec902f81a9b863f5
sungho <>
2010-10-20 09:19:41 +0000

    needed package for dijet ana

--- JulyExercise/scripts/setup36XHiSpectra.sh
+++ JulyExercise/scripts/setup36XHiSpectra.sh
@@ -15,6 +15,12 @@ cvs co UserCode/edwenger/VertexAnalyzer
 cvs co UserCode/edwenger/HiTrackSpectraAnalyzer
 cvs co UserCode/edwenger/HiTrkEffAnalyzer
 
+# needed for dijetana
+cvs co DataFormats/HeavyIonEvent
+cvs co HeavyIonsAnalysis/Configuration
+cvs co RecoHI/HiMuonAlgos
+
+
 # vertex constraint for refitting
 cvs co UserCode/ASYoon/PbPbTrackingTools/VertexConstraintProducer
 
