35e84b7100501c42a79924242f96b3ca6cabc37d
sungho <>
2010-11-05 14:54:59 +0000

    cleaned up

--- FirstHi2010/PbPbSpectraAna/scripts/setup39XHiSpectra.sh
+++ FirstHi2010/PbPbSpectraAna/scripts/setup39XHiSpectra.sh
@@ -5,27 +5,23 @@ cd $CMSSW_BASE/src
 # skiming and ana
 cvs co UserCode/FirstHi2010/PbPbSpectraAna
 
-# centrality related 
-#cvs co -r UserCode/CmsHi/JulyExercise/data/CentralityTables.db
-#cvs co -r HEAD RecoHI/HiCentralityAlgos
-#cvs co -r HEAD DataFormats/HeavyIonEvent
- 
 # analyzers
 cvs co UserCode/edwenger/VertexAnalyzer
 cvs co UserCode/edwenger/HiTrackSpectraAnalyzer
 cvs co UserCode/edwenger/HiTrkEffAnalyzer
+cvs co UserCode/SavedFMa/DiJetAna
+cvs co UserCode/SavedFMa/PatAlgos
+
+
+# tools
+cvs co UserCode/ASYoon/PbPbTrackingTools/VertexConstraintProducer
 
 # needed for dijetana
-#cvs co DataFormats/HeavyIonEvent
-#cvs co HeavyIonsAnalysis/Configuration
-#cvs co RecoHI/HiMuonAlgos
-
-# vertex constraint for refitting
-cvs co UserCode/ASYoon/PbPbTrackingTools/VertexConstraintProducer
 
 # setting and cleanup
 mv UserCode/edwenger .
 mv UserCode/ASYoon/PbPbTrackingTools .
 mv UserCode/FirstHi2010 .
+mv UserCode/SavedFMa/ Saved
 
 #scram b
