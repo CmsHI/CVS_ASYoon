e6b8720c19dd550b7e0097b8062698e079773a6e
sungho <>
2010-11-06 17:22:29 +0000

    yet another fix

--- FirstHi2010/PbPbSpectraAna/scripts/setup39XHiSpectra.sh
+++ FirstHi2010/PbPbSpectraAna/scripts/setup39XHiSpectra.sh
@@ -9,8 +9,8 @@ cvs co UserCode/ASYoon/FirstHi2010/PbPbSpectraAna
 cvs co UserCode/edwenger/HiVertexAnalyzer
 cvs co UserCode/edwenger/HiTrackSpectraAnalyzer
 cvs co UserCode/edwenger/HiTrkEffAnalyzer
-cvs co UserCode/SavedFMa/DiJetAna
-cvs co UserCode/SavedFMa/PatAlgos
+cvs co UserCode/SavedFMa/Saved/DiJetAna
+cvs co UserCode/SavedFMa/Saved/PatAlgos
 
 
 # tools
@@ -23,7 +23,7 @@ cvs co UserCode/ASYoon/PbPbTrackingTools/CaloCompatibleTrackSelector
 mv UserCode/edwenger .
 mv UserCode/ASYoon/PbPbTrackingTools .
 mv UserCode/ASYoon/FirstHi2010 .
-mv UserCode/SavedFMa/ Saved
+mv UserCode/SavedFMa/Saved .
 
 #
 #scram b
