ff0ba50f3ce16cf01aa414984e163b483c021359
sungho <>
2010-11-10 06:18:46 +0000

    centrality cut

--- FirstHi2010/PbPbSpectraAna/test/HiTrkAnaSkim_mc_2760GeV_cfg.py
+++ FirstHi2010/PbPbSpectraAna/test/HiTrkAnaSkim_mc_2760GeV_cfg.py
@@ -1,4 +1,7 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+import os
+
 process = cms.Process("ANASKIM")
 
 process.load('Configuration/StandardSequences/Services_cff')
@@ -8,20 +11,44 @@ process.load('Configuration/StandardSequences/MagneticField_AutoFromDBCurrent_cf
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.load('Configuration/EventContent/EventContent_cff')
 
+# ============= pre-setting ============================
+# setup 'standard'  options
+options = VarParsing.VarParsing ('standard')
+
+# my own variable
+options.register('centRange',
+                 "0To10", 
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "Centrality bin range")
+
+# get and parse the command line arguments
+options.parseArguments()
+
+
 # =============== 2.76 TeV MC Sample =====================
 
 process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring('/store/user/davidlw/Hydjet_Quenched_MinBias_2760GeV_STARTUP39V2_GEN_SIM_RAWDEBUG_390/Hydjet_Quenched_MinBias_2760GeV_STARTUP39V2_GEN_SIM_RECODEBUG_390/e91272bc2b91d5f6eefc8e1f2c112a67/hiReco_RAW2DIGI_RECO_2_3_qyi.root')
+   fileNames = cms.untracked.vstring('file:/home/sungho/sctch101/mc/firsthi2010/hiReco_RAW2DIGI_RECODEBUG_55_1_txh.root')
 )
 
 # =============== Other Statements =====================
 
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(2))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
-process.GlobalTag.globaltag = 'START39_V2::All'
+process.GlobalTag.globaltag = 'START39_V4HI::All' 
+
+# Needed for Centrality Table
+process.HeavyIonGlobalParameters = cms.PSet(
+    centralitySrc = cms.InputTag("hiCentrality"),
+    centralityVariable = cms.string("HFhits"),
+    nonDefaultGlauberModel = cms.string("")
+)
+from CmsHi.Analysis2010.CommonFunctions_cff import *
+overrideCentrality(process)
 
 process.configurationMetadata = cms.untracked.PSet(
-        version = cms.untracked.string('$Revision: 1.1 $'),
+        version = cms.untracked.string('$Revision: 1.2 $'),
             name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/ASYoon/FirstHi2010/PbPbSpectraAna/test/HiTrkAnaSkim_mc_2760GeV_cfg.py,v $'),
             annotation = cms.untracked.string('BPTX_AND + BSC_OR + !BSCHALO')
         )
@@ -31,42 +58,48 @@ process.TFileService = cms.Service("TFileService",
                                    )
 
 # =============== Import Sequences =====================
-process.load("FirstHi2010.PbPbSpectraAna.HiAnalysis_cff")
+process.load("FirstHi2010.PbPbSpectraAna.HiEventFilter_cff")
 process.load("FirstHi2010.PbPbSpectraAna.HiExtraReco_cff")
+process.load("FirstHi2010.PbPbSpectraAna.HiAnalysis_cff")
 
-from FirstHi2010.PbPbSpectraAna.hicustomise_cfi import *
-process = enableSIM(process)    # activate isGEN in analyzers
 
 # =============== Pat jet in HI ========================
 from Saved.DiJetAna.customise_cfi import *
 enableRECO(process,"MC","HI")
 
 # =============== Final Paths =====================
-process.extraReco_step   = cms.Path(process.hiextraReco + process.reco_extra)
-process.ana_step         = cms.Path(process.hiAnalysisSeq)
+process.eventFilter_step = cms.Path(process.eventFilter)
+process.extraReco_step   = cms.Path(process.eventFilter*(process.hiextraReco + process.reco_extra))
+process.ana_step         = cms.Path(process.eventFilter*process.hiAnalysisSeq)
 
 # =============== Customize =======================
+from FirstHi2010.PbPbSpectraAna.hicustomise_cfi import *
+process = enableSIM(process)    # activate isGEN in analyzers
 process = disableLowPt(process) # disable low pt pixel
-process = setAnaSeq(process,mode="ALL") # EffOnly, AnaOnly, ALL
+process = setAnaSeq(process,"ALL") # EffOnly, AnaOnly, ALL
+process = enableREDIGI(process) # to run on redigitized 
+process = whichCentBins(process,options.centRange)
 
 # =============== Output ================================
 process.load("FirstHi2010.PbPbSpectraAna.hianalysisSkimContent_cff")
-process.output = cms.OutputModule("PoolOutputModule",
-    process.analysisSkimContent,
-    #outputCommands = cms.untracked.vstring('keep *'),
-    #SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('eventFilter_step')),
-    dataset = cms.untracked.PSet(
-      dataTier = cms.untracked.string('AODSIM'),
-      filterName = cms.untracked.string('TrkAnaFilter')),
-    fileName = cms.untracked.string('trkAnaSkimAODSIM.root')
-)
 
-process.output_step = cms.EndPath(process.output)
+#process.output = cms.OutputModule("PoolOutputModule",
+#    #process.analysisSkimContent,
+#    outputCommands = cms.untracked.vstring('keep *'),
+#    SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('eventFilter_step')),
+#    dataset = cms.untracked.PSet(
+#      dataTier = cms.untracked.string('AODSIM'),
+#      filterName = cms.untracked.string('TrkAnaFilter')),
+#    fileName = cms.untracked.string('trkAnaSkimAODSIM_test.root')
+#)
+
+#process.output_step = cms.EndPath(process.output)
 
 # =============== Schedule =====================
 
 process.schedule = cms.Schedule(
+    process.eventFilter_step,
     process.extraReco_step,
     process.ana_step,
-    process.output_step
+    #process.output_step,
     )
