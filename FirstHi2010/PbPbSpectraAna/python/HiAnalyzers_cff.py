f2c307a5e596d2455cc9c64fcd7bbe2a6fa87a83
sungho <>
2010-11-10 06:12:46 +0000

    commissioning phase

--- FirstHi2010/PbPbSpectraAna/python/HiAnalyzers_cff.py
+++ FirstHi2010/PbPbSpectraAna/python/HiAnalyzers_cff.py
@@ -2,9 +2,16 @@ import FWCore.ParameterSet.Config as cms
 
 # clone event selection analyzer to run after successive steps
 
+
 # clone vertex analyzer to run with various inputs
 from edwenger.HiVertexAnalyzer.hivertexanalyzer_cfi import *
-selVtxAna = hivertexanalyzer.clone(vtxlabel=cms.untracked.InputTag("hiSelectedVertex"))
+preAdpVtxAna = hivertexanalyzer.clone(vtxlabel=cms.untracked.InputTag("hiPixelAdaptiveVertex"))
+preMedVtxAna = hivertexanalyzer.clone(vtxlabel=cms.untracked.InputTag("hiPixelMedianVertex"))
+preSelVtxAna = hivertexanalyzer.clone(vtxlabel=cms.untracked.InputTag("hiSelectedVertex"))
+
+postAdpVtxAna = hivertexanalyzer.clone(vtxlabel=cms.untracked.InputTag("hiPixelAdaptiveVertex"))
+postMedVtxAna = hivertexanalyzer.clone(vtxlabel=cms.untracked.InputTag("hiPixelMedianVertex"))
+postSelVtxAna = hivertexanalyzer.clone(vtxlabel=cms.untracked.InputTag("hiSelectedVertex"))
 
 # clone hitrack spectra ana to run with pixel and global
 from edwenger.HiTrackSpectraAnalyzer.hitrackspectraanalyzer_cfi import *
@@ -21,5 +28,17 @@ hirefitTrackAna = hitrackAna.clone(src=cms.untracked.InputTag("hirefitTracks")) 
 
 # tracking efficiency analyzer
 # defined in edwenger/HiTrkEffAnalyzer/python/hitrkEffAnalyzer_cff.py
+
+
+# Sequences
+preEvtSelVtxAna = cms.Sequence(preAdpVtxAna*
+                               preMedVtxAna*
+                               preSelVtxAna)
+
+postEvtSelVtxAna = cms.Sequence(postAdpVtxAna*
+                                postMedVtxAna*
+                                postSelVtxAna)
+                              
+
 
 
