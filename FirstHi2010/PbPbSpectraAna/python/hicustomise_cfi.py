033e3a9a9947defdaf6610fed251e5922bb257c5
sungho <>
2010-11-10 06:16:31 +0000

    centrality binning scenarios

--- FirstHi2010/PbPbSpectraAna/python/hicustomise_cfi.py
+++ FirstHi2010/PbPbSpectraAna/python/hicustomise_cfi.py
@@ -32,6 +32,37 @@ def disableEff(process):
     process.hiAnalysisSeq.remove(process.hitrkEffAna)
     return process
 
+def enableREDIGI(process):
+    process.hltMinBias.TriggerResultsTag=cms.InputTag('TriggerResults','','REDIGI')
+    process.hltJets.TriggerResultsTag=cms.InputTag('TriggerResults','','REDIGI')
+    process.hitrackAna.triglabel=cms.untracked.InputTag('TriggerResults','','REDIGI')
+    return process
+    
+
+def setCentBins(process,ci=0,cf=10):
+    if ci==cf:
+        print "No centrality selection"
+        process.eventFilter.remove(process.centFilter)
+    else: 
+        for i in range(ci,cf+1):
+            process.centFilter.selectedBins.append(i)
+        print "Centrality bins = ", process.centFilter.selectedBins
+    return process
+
+def whichCentBins(process,centRange):
+    if centRange=="0To10":
+        setCentBins(process,0,3)
+    elif centRange=="0To20":
+        setCentBins(process,0,7)
+    elif centRange=="20To40":
+        setCentBins(process,9,15)
+    elif centRange=="60To100":
+        setCentBins(process,25,39)
+    elif centRange=="ALL":
+        setCentBins(process,0,0)
+    return process
+            
+
 def setAnaSeq(process,mode="AnaOnly"):
     if mode=="AnaOnly":
         process = disableEff(process)
