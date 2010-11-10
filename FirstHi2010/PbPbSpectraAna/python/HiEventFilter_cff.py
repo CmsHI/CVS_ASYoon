41c5e872663af5b6a2df78df52c9d1addf661411
sungho <>
2010-11-10 06:14:44 +0000

    centrality cuts

--- FirstHi2010/PbPbSpectraAna/python/HiEventFilter_cff.py
+++ FirstHi2010/PbPbSpectraAna/python/HiEventFilter_cff.py
@@ -1,8 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoHI.HiCentralityAlgos.CentralityFilter_cfi import *
+from FirstHi2010.PbPbSpectraAna.hieventSelection_cff import *
+from FirstHi2010.PbPbSpectraAna.hihfCoincFilter_cff import *
+from FirstHi2010.PbPbSpectraAna.HiAnalyzers_cff import *
 
-centFilter1st = centralityFilter.clone(selectedBins = cms.vint32(0))
-centFilter2nd = centralityFilter.clone(selectedBins = cms.vint32(1,2))
-centFilter3rd = centralityFilter.clone(selectedBins = cms.vint32(3,4))
-centFilter4th = centralityFilter.clone(selectedBins = cms.vint32(5,6,7,8,9))
+# see FirstHi2010/PbPbSpectraAna/python/hicustomise_cfi.py 
+centFilter = centralityFilter.clone(selectedBins = cms.vint32())
+
+eventFilter = cms.Sequence(centFilter *
+                           preEvtSelVtxAna *
+                           minBiasBscFilter *    ## L1 and(or) HLT filter
+                           postEvtSelVtxAna *
+                           hfCoincFilter)
+
