5ebd0687c0aa467215deb89e674e94e8ccf9c719
sungho <>
2010-11-15 23:20:05 +0000

    common HI collision definition

--- FirstHi2010/PbPbSpectraAna/python/HiEventFilter_cff.py
+++ FirstHi2010/PbPbSpectraAna/python/HiEventFilter_cff.py
@@ -5,6 +5,9 @@ from FirstHi2010.PbPbSpectraAna.hieventSelection_cff import *
 from FirstHi2010.PbPbSpectraAna.hihfCoincFilter_cff import *
 from FirstHi2010.PbPbSpectraAna.HiAnalyzers_cff import *
 
+# Common HI ana
+from HeavyIonsAnalysis.Configuration.collisionEventSelection_cff import *
+
 # see FirstHi2010/PbPbSpectraAna/python/hicustomise_cfi.py 
 centFilter = centralityFilter.clone(selectedBins = cms.vint32())
 
