11bf5961c29a93bfd78ef810faa1a62ec5f6865b
sungho <>
2010-11-15 14:17:37 +0000

    verbose

--- FirstHi2010/PbPbSpectraAna/python/HiPFRereco_cff.py
+++ FirstHi2010/PbPbSpectraAna/python/HiPFRereco_cff.py
@@ -3,6 +3,9 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.ReconstructionHeavyIons_cff import *
 from RecoHI.Configuration.Reconstruction_hiPF_cff import *
 
+trackerDrivenElectronSeeds.TkColList = cms.VInputTag("hiSelectedTracks")
+particleFlow.vertexCollection = cms.InputTag("hiSelectedVertex")
+
 # Filter on reco::Track pt
 trkfilter = cms.EDFilter("PtMinTrackSelector",
                          src = cms.InputTag("hiSelectedTracks"),
@@ -22,4 +25,6 @@ pftrkfilter = cms.EDFilter("PtMinCandViewCloneSelector",
 rechits = cms.Sequence(siPixelRecHits * siStripMatchedRecHits)
 pfreco = cms.Sequence(rechits * heavyIonTracking * HiParticleFlowReco)
 
+
+# HI rereco with PF reco is only performed on events with high pt tracks
 rereco_seq = cms.Sequence(trkfilter * pfreco * pftrkfilter)
