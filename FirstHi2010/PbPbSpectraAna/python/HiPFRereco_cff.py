a87a20254c3ccfa695f05f17f80703d2fa00d9ef
sungho <>
2010-11-15 09:22:17 +0000

    pf re-reco and ana

--- FirstHi2010/PbPbSpectraAna/python/HiPFRereco_cff.py
+++ FirstHi2010/PbPbSpectraAna/python/HiPFRereco_cff.py
@@ -3,22 +3,23 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.ReconstructionHeavyIons_cff import *
 from RecoHI.Configuration.Reconstruction_hiPF_cff import *
 
-# Filter on PF candidate pt
-clones = cms.EDFilter("PtMinCandViewCloneSelector",
-                      src=cms.InputTag("particleFlow",""),
-                      ptMin=cms.double(3.0),
-                      filter=cms.bool(True)
-                      )
-
 # Filter on reco::Track pt
 trkfilter = cms.EDFilter("PtMinTrackSelector",
                          src = cms.InputTag("hiSelectedTracks"),
-                         ptMin = cms.double(3.0),
+                         ptMin = cms.double(4.0),
                          filter = cms.bool(True),
                          )
+
+
+# Filter on PF candidate pt
+pftrkfilter = cms.EDFilter("PtMinCandViewCloneSelector",
+                           src=cms.InputTag("particleFlow",""),
+                           ptMin=cms.double(4.0),
+                           filter=cms.bool(True)
+                           )
 
 
 rechits = cms.Sequence(siPixelRecHits * siStripMatchedRecHits)
 pfreco = cms.Sequence(rechits * heavyIonTracking * HiParticleFlowReco)
 
-rereco_seq = cms.Sequence(trkfilter * pfreco * clones)
+rereco_seq = cms.Sequence(trkfilter * pfreco * pftrkfilter)
