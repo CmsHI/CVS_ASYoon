d32def117f7387a8dcf1e008a83c85096edaafff
sungho <>
2010-11-17 13:49:45 +0000

    clean up

--- PbPbTrackingTools/CaloCompatibleTrackSelector/python/selectCaloCompatibleTrack_cfi.py
+++ PbPbTrackingTools/CaloCompatibleTrackSelector/python/selectCaloCompatibleTrack_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 selectCaloCompatibleTrack  = cms.EDProducer("CaloCompatibleTrackSelector",
                                    src = cms.InputTag("generalTracks"),
                                    srcPFCand = cms.InputTag("particleFlow",""),
-                                   keepAllTracks = cms.bool(False), ## if set to true tracks failing this filter are kept in the output
+                                   ptMin = cms.untracked.double(10),
                                    hasSimInfo =  cms.bool(False), 
                                    copyTrajectories = cms.untracked.bool(False), # true,  when doing retracking before
                                    copyExtras = cms.untracked.bool(True) ## set to false on AOD
