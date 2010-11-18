86f9476051824ac4b6a4bb7292c3315be0bb7f0e
sungho <>
2010-11-18 10:37:49 +0000

    change name

--- FirstHi2010/PbPbSpectraAna/python/HiTrackSelection_cff.py
+++ FirstHi2010/PbPbSpectraAna/python/HiTrackSelection_cff.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 from PbPbTrackingTools.CaloCompatibleTrackSelector.selectCaloCompatibleTrack_cfi import * 
 
-caloCompatibleTracks  = selectCaloCompatibleTrack.clone(src = cms.InputTag("hiSelectedTracks"),
-                                                        srcPFCand = cms.InputTag("particleFlow","","ANASKIM"),
-                                                        vertices = cms.InputTag("hiSelectedVertex"),
-                                                        ptMin = cms.untracked.double(10),
-                                                        selectFake = cms.untracked.bool(False))
+hiCaloCompTracks  = selectCaloCompatibleTrack.clone(src = cms.InputTag("hiSelectedTracks"),
+                                                    srcPFCand = cms.InputTag("particleFlow","","ANASKIM"),
+                                                    vertices = cms.InputTag("hiSelectedVertex"),
+                                                    ptMin = cms.untracked.double(10),
+                                                    selectFake = cms.untracked.bool(False))
