f7b753b5c5ff3f233388dca30ae5c28dfa675418
sungho <>
2010-11-19 20:31:55 +0000

    adding of hiGoodTrackSelectoin

--- FirstHi2010/PbPbSpectraAna/python/HiExtraReco_cff.py
+++ FirstHi2010/PbPbSpectraAna/python/HiExtraReco_cff.py
@@ -8,15 +8,16 @@ from FirstHi2010.PbPbSpectraAna.HiPFRereco_cff import *
 from edwenger.HiTrkEffAnalyzer.hipfCandAnalyzer_cff import *
 from edwenger.HiTrkEffAnalyzer.TrackSelections_cff import *
 
+
+# Extra reco 
+hiextraReco = cms.Sequence(hiLowPtPixelTracks)
+
 # PF re-reco for events with pT>pT'
 hipfReReco = cms.Sequence(rereco_seq*
                           hipfCandAnalyzer)
 
-# Extra reco 
-hiextraReco = cms.Sequence(hiLowPtPixelTracks*
-                           hiGoodTracksSelection)
-
 
 # Extra track selections/refit/etc..
-hiextraTrack = cms.Sequence(hiCaloCompTracks
+hiextraTrack = cms.Sequence(hiGoodTracksSelection
+                            *hiCaloCompTracks
                             *hipfCandAnalyzer_test)
