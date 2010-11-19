5005f1041fad83cc71e27f472b811e9b180c73ab
sungho <>
2010-11-19 13:36:14 +0000

    generation of higood tracks

--- FirstHi2010/PbPbSpectraAna/python/HiExtraReco_cff.py
+++ FirstHi2010/PbPbSpectraAna/python/HiExtraReco_cff.py
@@ -6,15 +6,17 @@ from FirstHi2010.PbPbSpectraAna.HiTrackRefit_cff import *
 from FirstHi2010.PbPbSpectraAna.HiTrackSelection_cff import *
 from FirstHi2010.PbPbSpectraAna.HiPFRereco_cff import *
 from edwenger.HiTrkEffAnalyzer.hipfCandAnalyzer_cff import *
-
+from edwenger.HiTrkEffAnalyzer.TrackSelections_cff import *
 
 # PF re-reco for events with pT>pT'
 hipfReReco = cms.Sequence(rereco_seq*
                           hipfCandAnalyzer)
 
 # Extra reco 
-hiextraReco = cms.Sequence(hiLowPtPixelTracks)
+hiextraReco = cms.Sequence(hiLowPtPixelTracks*
+                           hiGoodTracksSelection)
 
 
 # Extra track selections/refit/etc..
-hiextraTrack = cms.Sequence(hitrackRefit + hiCaloCompTracks)
+hiextraTrack = cms.Sequence(hiCaloCompTracks
+                            *hipfCandAnalyzer_test)
