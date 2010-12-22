a2b3d29bb2fe88d627b593b43dce6a7f5a436613
sungho <>
2010-12-22 12:02:38 +0000

    fix

--- MCGeneration/PPSpectraPythiaTunes/scripts/savePythiaSummaryOnlyPY8.sh
+++ MCGeneration/PPSpectraPythiaTunes/scripts/savePythiaSummaryOnlyPY8.sh
@@ -2,7 +2,7 @@
 
 while read inputline
     do 
-       filename="$(echo $inputline | cut -d "/" -f11)"
+       filename="$(echo $inputline | cut -d "/" -f1)"
        outputfile="$PWD/$filename/res/CMSSW_1.stdout"
        echo "file to be looked at $outputfile"
        #begin="$(cat -n $outputfile  | grep "cmsRun started" | awk '{print $1}')"
