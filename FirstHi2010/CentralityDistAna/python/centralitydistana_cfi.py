9792d71dc22c542a9b3d7d6d835fb451c09435fe
sungho <>
2011-01-20 20:35:21 +0000

    needed centrality (not in use now)

--- FirstHi2010/CentralityDistAna/python/centralitydistana_cfi.py
+++ FirstHi2010/CentralityDistAna/python/centralitydistana_cfi.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 centbindist = cms.EDAnalyzer('CentralityDistAna',
-                             neededCentBins = cms.untracked.vint32(0,1,3,11,19,39), # 0~5% (0,1), 5~10% (2,3), 10~30% (4,11) etc..
+           neededCentBins = cms.untracked.vint32(0,1,3,11,19,39), # 0~5% (0,1), 5~10% (2,3), 10~30% (4,11) etc..
 )
