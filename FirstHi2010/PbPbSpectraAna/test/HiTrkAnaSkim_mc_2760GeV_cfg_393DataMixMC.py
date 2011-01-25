0c3aa73e36bbc83bf666db5ac8f99bd018a9c1a0
sungho <>
2011-01-25 14:57:23 +0000

    data centrality table with overrite option

--- FirstHi2010/PbPbSpectraAna/test/HiTrkAnaSkim_mc_2760GeV_cfg_393DataMixMC.py
+++ FirstHi2010/PbPbSpectraAna/test/HiTrkAnaSkim_mc_2760GeV_cfg_393DataMixMC.py
@@ -46,14 +46,14 @@ process.GlobalTag.globaltag = 'START39_V4HI::All'
 process.HeavyIonGlobalParameters = cms.PSet(
     centralitySrc = cms.InputTag("hiCentrality"),
     centralityVariable = cms.string("HFhits"),
-    nonDefaultGlauberModel = cms.string("Hydjet_2760GeV")
+    nonDefaultGlauberModel = cms.string("")
 )
 from CmsHi.Analysis2010.CommonFunctions_cff import *
-#overrideCentrality(process)
+overrideCentrality(process)
 
 process.configurationMetadata = cms.untracked.PSet(
-        version = cms.untracked.string('$Revision: 1.7 $'),
-            name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/ASYoon/FirstHi2010/PbPbSpectraAna/test/HiTrkAnaSkim_mc_2760GeV_cfg_393.py,v $'),
+        version = cms.untracked.string('$Revision: 1.1 $'),
+            name = cms.untracked.string('$Source: /cvs/CMSSW/UserCode/ASYoon/FirstHi2010/PbPbSpectraAna/test/HiTrkAnaSkim_mc_2760GeV_cfg_393DataMixMC.py,v $'),
             annotation = cms.untracked.string('BPTX_AND + BSC_OR + !BSCHALO')
         )
 
