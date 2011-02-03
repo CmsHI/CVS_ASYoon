641781eacebb06ce843047bfc3fb5285b8c3894a
sungho <>
2011-02-03 16:19:24 +0000

    kill those stuck in Schedule and resubmit

---
+++ MISC/scripts/kill_n_resubmit_sheduled.sh
@@ -0,0 +1,31 @@
+# listOfJobs.txt is a list of crab directory names
+
+while read inputline
+    do
+       filename="$(echo $inputline | cut -d "/" -f1)"
+       echo "file name = $filename"
+       crab -status -c $filename > ${filename}_output.txt
+       begin="$(cat -n ${filename}_output.txt  | grep "List of jobs Scheduled" | awk '{print $1}')"
+       if [ -z "$begin" ]
+	   then
+	   echo "No scheduled jobs!"
+       else
+	   echo "Yes schedule jobs!"
+	   last="$(cat -n ${filename}_output.txt  | grep "Jobs Done" | awk '{print $1}')"
+	   echo "new begins at $newbegin and ends at $newlast"
+	   newbegin=$begin
+	   newlast=`expr $begin + 1`
+	   range="$newbegin, ${newlast}p"
+	   echo "real range is $range"
+	   jobrange="$(sed -n "$(echo $range)" ${filename}_output.txt | awk '{print $5}')"
+	   echo "jobs to be killed and resumitted = $jobrange"
+	   #echo crab -kill $jobrange -c $filename
+           crab -kill $jobrange -c $filename
+	   sleep 5
+	   #echo crab -resubmit $jobrange -c $filename
+           crab -resubmit $jobrange -c $filename
+       fi
+       rm ${filename}_output.txt
+       sleep 5
+done < listOfJobs.txt
+
