823a119b58c3a3800138c6fbd489c1fd20c5aa6f
sungho <>
2011-02-04 18:40:04 +0000

    0 to 10 range

--- MCGeneration/PPSpectraPythiaTunes/python/customiseGEN_cfi.py
+++ MCGeneration/PPSpectraPythiaTunes/python/customiseGEN_cfi.py
@@ -8,6 +8,7 @@ def getPy6ProcParameters(genTypePtHatRange):
         'NSD_0_to_5':['MSEL=1','CKIN(3)=0','CKIN(4)=5'], 
         'NSD_5_to_10':['MSEL=1','CKIN(3)=5','CKIN(4)=10'],
         'NSD_10_to_20':['MSEL=1','CKIN(3)=10','CKIN(4)=20'],
+        'NSD_0_to_10':['MSEL=1','CKIN(3)=0','CKIN(4)=10'],
         'NSD_0_to_15':['MSEL=1','CKIN(3)=0','CKIN(4)=15'],
         'NSD_0_to_20':['MSEL=1','CKIN(3)=0','CKIN(4)=20'],
         'NSD_15_to_20':['MSEL=1','CKIN(3)=15','CKIN(4)=20'],
