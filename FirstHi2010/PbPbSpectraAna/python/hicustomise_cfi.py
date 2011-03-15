5123734e453114b183de386494cb3acf23912ec0
sungho <>
2011-03-15 17:38:40 +0000

    fix for 384p4

--- FirstHi2010/PbPbSpectraAna/python/hicustomise_cfi.py
+++ FirstHi2010/PbPbSpectraAna/python/hicustomise_cfi.py
@@ -69,7 +69,8 @@ def runOnCore(process):
     return process
 
 def runOn384p2(process):
-    list = ['HLT_HIMinBiasCalo','HLT_HIJet35U','HLT_HIJet50U','HLT_HIJet75U','HLT_HIJet90U']
+    #list = ['HLT_HIMinBiasCalo','HLT_HIJet35U','HLT_HIJet50U','HLT_HIJet75U','HLT_HIJet90U']
+    list = ['HLT_HIMinBiasCalo','HLT_HIJet35U']
     print "runOn384p2 option is enabled (HLT_HIMinBiasCalo and HISIGNAL)!"
     print "hlt list for track analyzer = ",list
     process.hltMinBias.HLTPaths=cms.vstring('HLT_HIMinBiasCalo')  # HLT_HIMinBiasBSC is not available
@@ -92,9 +93,9 @@ def runOn384p2(process):
     process.hihightrkval_fake.useQaulityStr =cms.untracked.bool(False)
     process.higoodtrkval.useQaulityStr =cms.untracked.bool(False)
     process.higoodtrkval_fake.useQaulityStr =cms.untracked.bool(False)
-    process.preTrgAna.trignames = cms.untracked.bool(False)
-    process.postTrgAna.trignames = cms.untracked.bool(False)
-    process.postEvtAna.trignames = cms.untracked.bool(False)
+    process.preTrgAna.trignames = cms.untracked.vstring(list)
+    process.postTrgAna.trignames = cms.untracked.vstring(list)
+    process.postEvtAna.trignames = cms.untracked.vstring(list)
     return process
 
 def runOn393(process):
