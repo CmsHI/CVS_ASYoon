6f62f86d317327167b6684382cc4b67d240325a0
sungho <>
2011-04-18 16:04:48 +0000

    icPu5patJets

--- FirstHi2010/CentralityDistAna/python/centralitydistana_cfi.py
+++ FirstHi2010/CentralityDistAna/python/centralitydistana_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 centbindist = cms.EDAnalyzer('CentralityDistAna',
-        jsrc = cms.untracked.InputTag("patJets"),
+        jsrc = cms.untracked.InputTag("icPu5patJets"),
         neededCentBins = cms.untracked.vint32(0,1,3,11,19,39), # 0~5% (0,1), 5~10% (2,3), 10~30% (4,11) etc..
         useJetThreshold = cms.untracked.bool(False) # fill histogram for Jet ET> ET'
 )
