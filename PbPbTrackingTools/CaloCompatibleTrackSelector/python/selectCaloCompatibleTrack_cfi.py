b64d25da9f2990402c54ff7384eaa250b9070f2c
sungho <>
2011-05-01 21:10:20 +0000

    compatibility check added

--- PbPbTrackingTools/CaloCompatibleTrackSelector/python/selectCaloCompatibleTrack_cfi.py
+++ PbPbTrackingTools/CaloCompatibleTrackSelector/python/selectCaloCompatibleTrack_cfi.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 selectCaloCompatibleTrack  = cms.EDProducer("CaloCompatibleTrackSelector",
-                                   src = cms.InputTag("generalTracks"),
-                                   srcPFCand = cms.InputTag("particleFlow",""),
-                                   ptMin = cms.untracked.double(10),
-                                   hasSimInfo =  cms.bool(False), 
-                                   funcCaloComp = cms.string("(x>14) * (1.2*pow(TMath::Abs(x-10),8.7/9)) * (2+1./(exp(-1*(x-13))-1))"), # a function that defines track-calo (in)compatible region
-                                   copyTrajectories = cms.untracked.bool(False), # true,  when doing retracking before
-                                   copyExtras = cms.untracked.bool(True) ## set to false on AOD
-                                   )
+              src = cms.InputTag("generalTracks"),
+              srcPFCand = cms.InputTag("particleFlow",""),
+              ptMin = cms.untracked.double(10),
+              hasSimInfo =  cms.bool(False),
+              funcCaloComp = cms.string("(x>10) * (1.2*pow(TMath::Abs(x-10),6.6/9)) * (2+1.4/(exp(-10*(x-9))-1))"), # a function that defines track-calo (in)compatible region
+              copyTrajectories = cms.untracked.bool(False), # true,  when doing retracking before
+              copyExtras = cms.untracked.bool(True) ## set to false on AOD
+)
