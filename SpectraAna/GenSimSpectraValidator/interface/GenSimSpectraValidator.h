4957aa9518cdb097b3846f7520746a8fbccd4c88
sungho <>
2011-07-05 16:17:09 +0000

    more information regarding sim tracks

--- SpectraAna/GenSimSpectraValidator/interface/GenSimSpectraValidator.h
+++ SpectraAna/GenSimSpectraValidator/interface/GenSimSpectraValidator.h
@@ -12,6 +12,9 @@
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 #include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
@@ -20,7 +23,19 @@
 #include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
 #include "SimDataFormats/TrackingAnalysis/interface/TrackingParticleFwd.h"
 
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
+
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+
+#include "SimTracker/Records/interface/TrackAssociatorRecord.h"
+#include "DataFormats/SiPixelDetId/interface/PixelSubdetector.h"
+#include "DataFormats/SiPixelDetId/interface/PXBDetId.h"
+#include "DataFormats/SiPixelDetId/interface/PXFDetId.h"
+#include "DataFormats/SiPixelDetId/interface/PixelEndcapName.h"
+#include "Geometry/CommonDetUnit/interface/GeomDetUnit.h"
+
+#include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
+
 
 // ROOT includes
 #include "TNtuple.h"
@@ -39,8 +54,12 @@ class GenSimSpectraValidator : public edm::EDAnalyzer {
 
  private:
    virtual void beginJob() ;
+   virtual void beginRun(const edm::Run&, const edm::EventSetup&);
    virtual void analyze(const edm::Event&, const edm::EventSetup&);
    virtual void endJob() ;
+
+   std::pair<bool,bool> isAccepted(const TrackingParticle&);
+   int getLayerId(const PSimHit&);
 
    // ----------member data --------------------------- 
    edm::InputTag gsrc_;
@@ -49,12 +68,18 @@ class GenSimSpectraValidator : public edm::EDAnalyzer {
    double  etaMax_;
 
    edm::Service<TFileService> fs;
+
+   const TrackerGeometry * theTracker;
    
    // root objects
    TNtuple *nt_gen, *nt_sim;
    TNtuple *nt_sim_vtx;
    
-   
+   enum { BPix1=0, BPix2=1, BPix3=2,
+	  FPix1_neg=3, FPix2_neg=4,
+	  FPix1_pos=5, FPix2_pos=6,
+	  nLayers=7};
+
    
 };
 
--- SpectraAna/GenSimSpectraValidator/src/GenSimSpectraValidator.cc
+++ SpectraAna/GenSimSpectraValidator/src/GenSimSpectraValidator.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Andre Yoon,32 4-A06,+41227676980,
 //         Created:  Sun Nov 21 14:51:53 CET 2010
-// $Id: GenSimSpectraValidator.cc,v 1.3 2011/01/20 20:04:12 sungho Exp $
+// $Id: GenSimSpectraValidator.cc,v 1.4 2011/07/04 12:32:26 sungho Exp $
 //
 //
 
@@ -53,6 +53,9 @@ GenSimSpectraValidator::analyze(const edm::Event& iEvent, const edm::EventSetup&
    using namespace std;
    using namespace reco;
 
+   // Get PDTable
+   edm::ESHandle<ParticleDataTable> pdt_;
+   iSetup.getData( pdt_);
    
    // Gen level ----------------------------------------------------------------
    Handle<GenParticleCollection> genParticles;
@@ -90,8 +93,16 @@ GenSimSpectraValidator::analyze(const edm::Event& iEvent, const edm::EventSetup&
 	 parentId = (*(sim.parentVertex()->sourceTracks_begin()))->pdgId(); // PDG id of parent particle
       } 
 
+      std::pair<bool,bool> acc = isAccepted(sim);
+      int isAcc = (acc.first || acc.second) ? 1 : 0;
+
+      const ParticleData * pd = pdt_->particle(abs(sim.pdgId()));
+      double lifetime = pd->lifetime().value();
+      //std::cout<<"lifetime = "<<lifetime<<" 2 = "<<pd->lifetime()<<" name = "<<pd->name()
+      //<<" mass = "<<pd->mass()<<std::endl;
+
       nt_sim->Fill(sim.eta(),sim.phi(),sim.pt(),sim.pdgId(),sim.status(),fabs(sim.vertex().z()),sqrt(sim.vertex().perp2()),
-		   sim.parentVertex()->nSourceTracks(),parentId);
+		   sim.parentVertex()->nSourceTracks(),parentId,isAcc);
    }
 
 
@@ -109,13 +120,25 @@ GenSimSpectraValidator::analyze(const edm::Event& iEvent, const edm::EventSetup&
 }
 
 
+// ------------ method called at the beginning of each new run  ------------
+void
+GenSimSpectraValidator::beginRun(const edm::Run& r, const edm::EventSetup& es)
+{
+
+   // Get tracker geometry
+   edm::ESHandle<TrackerGeometry> tracker;
+   es.get<TrackerDigiGeometryRecord>().get(tracker);
+   theTracker = tracker.product();
+
+}
+
 // ------------ method called once each job just before starting event loop  ------------
 void 
 GenSimSpectraValidator::beginJob()
 {
    // tracks
    nt_gen = fs->make<TNtuple>("nt_gen","gen level information","geta:gphi:gpt:gpdgid");
-   nt_sim = fs->make<TNtuple>("nt_sim","sim level information","seta:sphi:spt:spdgid:sstatus:sdz:sd0:snptrk:sptrkid");
+   nt_sim = fs->make<TNtuple>("nt_sim","sim level information","seta:sphi:spt:spdgid:sstatus:sdz:sd0:snptrk:sptrkid:sacc");
 
    // vertices
    //nt_sim_vtx = fs->make<TNtuple>("nt_sim_vtx","sim level vtx information","vx:vy:vz:pdgid:status");
@@ -125,6 +148,76 @@ GenSimSpectraValidator::beginJob()
 void 
 GenSimSpectraValidator::endJob() {
 }
+
+// ------------
+std::pair<bool,bool> 
+GenSimSpectraValidator::isAccepted(const TrackingParticle & tp)
+{
+   std::vector<bool> f(nLayers, false);
+
+   const std::vector<PSimHit> & simHits = tp.trackPSimHit(DetId::Tracker);
+  
+   for(std::vector<PSimHit>::const_iterator simHit = simHits.begin();
+       simHit!= simHits.end(); simHit++)
+      {
+	 int id = getLayerId(*simHit);
+
+	 if(id != -1)
+	    f[id] = true;
+      }
+
+  bool canBeTriplet =
+     ( (f[BPix1] && f[BPix2]     && f[BPix3]) ||
+       (f[BPix1] && f[BPix2]     && f[FPix1_pos]) ||
+       (f[BPix1] && f[BPix2]     && f[FPix1_neg]) ||
+       (f[BPix1] && f[FPix1_pos] && f[FPix2_pos]) ||
+       (f[BPix1] && f[FPix1_neg] && f[FPix2_neg]) );
+
+  bool canBePair =
+     ( (f[BPix1] && f[BPix2]) ||
+       (f[BPix1] && f[BPix3]) ||
+       (f[BPix2] && f[BPix3]) ||
+       (f[BPix1] && f[FPix1_pos]) ||
+       (f[BPix1] && f[FPix1_neg]) ||
+       (f[BPix1] && f[FPix2_pos]) ||
+       (f[BPix1] && f[FPix2_neg]) ||
+       (f[BPix2] && f[FPix1_pos]) ||
+       (f[BPix2] && f[FPix1_neg]) ||
+       (f[BPix2] && f[FPix2_pos]) ||
+       (f[BPix2] && f[FPix2_neg]) ||
+       (f[FPix2_neg] && f[FPix2_neg]) ||
+       (f[FPix2_pos] && f[FPix2_pos]) );
+
+  return std::pair<bool,bool>(canBeTriplet, canBePair);
+}
+
+
+// ------------
+int 
+GenSimSpectraValidator::getLayerId(const PSimHit & simHit)
+{
+
+   unsigned int id = simHit.detUnitId();
+
+   if(theTracker->idToDetUnit(id)->subDetector() ==
+      GeomDetEnumerators::PixelBarrel)
+      {
+	 PXBDetId pid(id);
+	 return pid.layer() - 1; // 0, 1, 2
+      }
+
+   if(theTracker->idToDetUnit(id)->subDetector() ==
+      GeomDetEnumerators::PixelEndcap)
+      {
+	 PXFDetId pid(id);
+	 return BPix3 + ((pid.side()-1) << 1) + pid.disk(); // 3 -
+      }
+
+
+   // strip
+   return -1;
+}
+
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(GenSimSpectraValidator);
