6aae0dced04d6553c4b827a21152bdd264c0eb3a
sungho <>
2011-08-07 14:39:04 +0000

    all validator is removed for light cfg

--- FirstHi2010/PbPbSpectraAna/python/hicustomise_cfi.py
+++ FirstHi2010/PbPbSpectraAna/python/hicustomise_cfi.py
@@ -64,8 +64,10 @@ def enableSIM(process):
 
 def runWithLightCfg(process):
     print "Light cfg: following modules are removed"
-    print "hihightrkval*, higloosetrkval*, hipredttrkval*"
+    print "higoodtrkval*, hihightrkval*, higloosetrkval*, hipredttrkval*"
     print "hihightrackAna*, higoodlooseAna*"
+    process.hiAnalysisSeq.remove(process.higoodtrkval)
+    process.hiAnalysisSeq.remove(process.higoodtrkval_pt80)
     process.hiAnalysisSeq.remove(process.hihightrkval)
     process.hiAnalysisSeq.remove(process.hihightrkval_pt80)
     process.hiAnalysisSeq.remove(process.higloosetrkval)
