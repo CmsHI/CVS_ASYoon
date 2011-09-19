54692e283231b76b46814cc3e3bd232a8faead9c
sungho <>
2011-09-19 19:35:02 +0000

    cleaned

--- MISC/python/ExtraReco_cff.py
+++ MISC/python/ExtraReco_cff.py
@@ -4,7 +4,6 @@ from Saved.DiJetAna.TrackSelection_cff import *
 from HeavyIonsAnalysis.Configuration.analysisProducers_cff import *
 from RecoHI.HiCentralityAlgos.CentralityBin_cfi import *
 #from edwenger.HiTrkEffAnalyzer.TrackSelections_cff import *
-#from MNguyen.iterTracking.TrackSelections_cff import *
 
 dj_reco_extra = cms.Sequence(centralityBin# *
     #hiGoodTracksSelection
