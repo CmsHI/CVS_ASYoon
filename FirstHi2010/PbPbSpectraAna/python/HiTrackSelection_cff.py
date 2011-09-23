884b8591950e5fc2297ca3d813036862838ece14
sungho <>
2011-09-23 09:47:09 +0000

    hiPtDeptOptCaloTracks

--- FirstHi2010/PbPbSpectraAna/python/HiTrackSelection_cff.py
+++ FirstHi2010/PbPbSpectraAna/python/HiTrackSelection_cff.py
@@ -3,4 +3,14 @@ import FWCore.ParameterSet.Config as cms
 from PbPbTrackingTools.CaloCompatibleTrackSelector.selectCaloCompatibleTrack_cfi import * 
 
 hiGeneralCaloTracks = selectCaloCompatibleTrack.clone(src = cms.InputTag("hiGeneralTracks"))
+
+
+
+# This is for hiPtDepOptCaloTracks, which is the merged track collection of
+# hiPtDeptOptCaloPreFst: calo compatible + loose selection
+# hiPtDeptOptCaloPreSnd: calo incompatible + tight (hiGoodTight) selection
+
+hiPtDeptOptCaloPreFst = selectCaloCompatibleTrack.clone(src = cms.InputTag("hiGeneralGlobalPrimTracks"),
+                                                        applyPtDepCut = cms.untracked.bool(True))
+hiPtDeptOptCaloPreSnd = hiPtDeptOptCaloPreFst.clone(reverseSel = cms.untracked.bool(True))
                                                       
