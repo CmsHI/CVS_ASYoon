50dab6bd10c8e52f6e4f121a71b37fcfe03441ae
sungho <>
2011-10-11 19:31:11 +0000

    updated to include CaloTower based calo-cut

--- FirstHi2010/PbPbSpectraAna/python/HiTrackSelection_cff.py
+++ FirstHi2010/PbPbSpectraAna/python/HiTrackSelection_cff.py
@@ -4,13 +4,26 @@ from PbPbTrackingTools.CaloCompatibleTrackSelector.selectCaloCompatibleTrack_cfi
 
 hiGeneralCaloTracks = selectCaloCompatibleTrack.clone(src = cms.InputTag("hiGeneralTracks"))
 
-
-
+#============================================================================
 # This is for hiPtDepOptCaloTracks, which is the merged track collection of
 # hiPtDeptOptCaloPreFst: calo compatible + loose selection
 # hiPtDeptOptCaloPreSnd: calo incompatible + tight (hiGoodTight) selection
+# (based on Energy Sum/track pT ratio with PF)
+#============================================================================
 
 hiPtDeptOptCaloPreFst = selectCaloCompatibleTrack.clone(src = cms.InputTag("hiGeneralGlobalPrimTracks"),
                                                         applyPtDepCut = cms.untracked.bool(True))
 hiPtDeptOptCaloPreSnd = hiPtDeptOptCaloPreFst.clone(reverseSel = cms.untracked.bool(True))
-                                                      
+
+#============================================================================                                                      
+# This is for hiPtDepOptCaloTracks, which is the merged track collection of
+# hiOptCaloPreFst: calo compatible + loose selection
+# hiOptCaloPreSnd: calo incompatible + tight (hiGoodTight) selection
+# (based on dR matchin with CaloTower)
+#===========================================================================
+
+from PbPbTrackingTools.HiCaloCompatibleTrackSelector.selectHiCaloCompatibleTrack_cfi import *
+
+hiOptCaloPreFst = selectHiCaloCompatibleTrack.clone(src = cms.InputTag("hiGeneralGlobalPrimTracks"),
+                                                    applyPtDepCut = cms.untracked.bool(True))
+hiOptCaloPreSnd = hiOptCaloPreFst.clone(reverseSel = cms.untracked.bool(True))
