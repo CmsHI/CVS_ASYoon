ee995cfb1dc7e4a9bccd041512cd3ca6710e21a5
sungho <>
2011-10-11 19:24:44 +0000

    SealModules is needed so that the selector is registered

---
+++ PbPbTrackingTools/HiCaloCompatibleTrackSelector/src/SealModules.cc
@@ -0,0 +1,12 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "PbPbTrackingTools/HiCaloCompatibleTrackSelector/interface/HiCaloCompatibleTrackSelector.h"
+
+
+
+using reco::modules::HiCaloCompatibleTrackSelector;
+
+
+DEFINE_FWK_MODULE(HiCaloCompatibleTrackSelector);
