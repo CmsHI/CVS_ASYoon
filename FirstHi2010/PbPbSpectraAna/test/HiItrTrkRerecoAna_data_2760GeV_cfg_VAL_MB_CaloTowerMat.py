98a721933d977d86a6a91bb88da6411475ab96b2
sungho <>
2011-10-12 22:39:07 +0000

    var.out

--- FirstHi2010/PbPbSpectraAna/test/HiItrTrkRerecoAna_data_2760GeV_cfg_VAL_MB_CaloTowerMat.py
+++ FirstHi2010/PbPbSpectraAna/test/HiItrTrkRerecoAna_data_2760GeV_cfg_VAL_MB_CaloTowerMat.py
@@ -11,7 +11,7 @@ myLumis = LumiList.LumiList(filename = 'json.txt').getCMSSWString().split(',')
 ivars = VarParsing.VarParsing('standard')
 ivars.register('initialEvent',mult=ivars.multiplicity.singleton,info="for testing")
 ivars.files = "dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/hidata/HIRun2010/HICorePhysics/RECO/PromptReco-v3/000/151/076/328420FD-4FF0-DF11-B358-001D09F241B9.root"
-ivars.output = 'trkhists.root'
+ivars.output = 'trkhistsDATA.root'
 ivars.maxEvents = 30
 ivars.initialEvent = 1
 
@@ -71,13 +71,13 @@ from CmsHi.Analysis2010.CommonFunctions_cff import *
 overrideCentrality(process)
 
 process.configurationMetadata = cms.untracked.PSet(
-  version = cms.untracked.string('$Revision: 1.1 $'),
+  version = cms.untracked.string('$Revision: 1.2 $'),
   name = cms.untracked.string('$Source: /cvs/CMSSW/UserCode/ASYoon/FirstHi2010/PbPbSpectraAna/test/HiItrTrkRerecoAna_data_2760GeV_cfg_VAL_MB_CaloTowerMat.py,v $'),
   annotation = cms.untracked.string('BPTX_AND + BSC_OR + !BSCHALO')
   )
 
 process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string('trkhistsDATA.root')
+                                   fileName = cms.string(ivars.output)
                                    )
 
 # =============== Import Sequences =====================
