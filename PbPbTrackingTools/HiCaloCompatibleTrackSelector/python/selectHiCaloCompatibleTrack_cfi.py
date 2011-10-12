b4e5625dad02e0d347ceaf9aa8187557da29af98
sungho <>
2011-10-12 15:12:06 +0000

    calo tower input tag and updated parameterization

--- PbPbTrackingTools/HiCaloCompatibleTrackSelector/python/selectHiCaloCompatibleTrack_cfi.py
+++ PbPbTrackingTools/HiCaloCompatibleTrackSelector/python/selectHiCaloCompatibleTrack_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 selectHiCaloCompatibleTrack  = cms.EDProducer("HiCaloCompatibleTrackSelector",
               src = cms.InputTag("generalTracks"),
-              srcTower = cms.InputTag(""),
+              srcTower = cms.InputTag("towerMaker"),
               applyCaloComp = cms.untracked.bool(True),  # if false, every track is kept (better not to run the PF to begin with)
               applyPtDepCut = cms.untracked.bool(False), # apply pt denpedent (funcCaloComp) cut !
               trkPtMin = cms.untracked.double(20.0),                                              
@@ -10,12 +10,12 @@ selectHiCaloCompatibleTrack  = cms.EDProducer("HiCaloCompatibleTrackSelector",
               trkEtaMax = cms.untracked.double(2.4),
               towerPtMin = cms.untracked.double(5.0),
               matchConeRadius = cms.untracked.double(0.087),
-              caloCut = cms.untracked.double(0.2), # (calo energy sum/track pt) > caloCut
+              caloCut = cms.untracked.double(0.3), # (calo energy sum/track pt) > caloCut (0 - loose, 1.0 - tight)
               copyTrajectories = cms.untracked.bool(False), # true,  when doing retracking before
               copyExtras = cms.untracked.bool(True), ## set to false on AOD
               useQaulityStr=cms.untracked.bool(False),
               qualityString=cms.untracked.string("highPurity"),
               hasSimInfo =  cms.bool(False),
-              funcCaloComp = cms.string("0.087/(1.0+exp(-0.28*(x-20.)))"), # root syntax, pt dependent calo-compatibility cut
+              funcCaloComp = cms.string("0.087/(1.0+0.1*exp(-0.28*(x-20.)))"), # root syntax, pt dependent calo-compatibility cut
               reverseSel=cms.untracked.bool(False) # reverse the selection!                                             
 )
