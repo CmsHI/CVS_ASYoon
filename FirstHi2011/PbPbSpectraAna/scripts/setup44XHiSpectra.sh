613d21d354f715a2268778f09ce26d451b94a62b
sungho <>
2011-11-29 13:55:06 +0000

    updated script

--- FirstHi2011/PbPbSpectraAna/scripts/setup44XHiSpectra.sh
+++ FirstHi2011/PbPbSpectraAna/scripts/setup44XHiSpectra.sh
@@ -8,6 +8,9 @@ cvs co UserCode/ASYoon/FirstHi2010/CentralityDistAna
 
 # common HI ana stuffs
 cvs co -r V01-02-09 HeavyIonsAnalysis/Configuration
+cvs co CondFormats/HIObjects
+cvs co DataFormats/HeavyIonEvent
+cvs co RecoHI/HiCentralityAlgos
 
 # hcal spike cleaning
 cvs co -d HcalFilter UserCode/MTonjes/HcalFilter
@@ -17,10 +20,15 @@ cvs co UserCode/edwenger/HiVertexAnalyzer
 cvs co UserCode/edwenger/HiTrackSpectraAnalyzer
 cvs co UserCode/edwenger/HiTrkEffAnalyzer
 cvs co UserCode/edwenger/HiEvtSelAnalyzer
+cvs co UserCode/ASYoon/SpectraAna/GenSimSpectraValidator
 
-
-# for jet
-
+# for jet (icPu5 pat jet)
+cvs co -d CmsHi/JetAnalysis UserCode/CmsHi/JetAnalysis
+rm CmsHi/JetAnalysis/python/EGammaAnalyzers_cff.py # tmp fix
+rm CmsHi/JetAnalysis/python/ExtraEGammaReco_cff.py # tmp fix
+rm CmsHi/JetAnalysis/python/EventSelection_cff.py # tmp fix
+rm CmsHi/JetAnalysis/python/ExtraPfReco_cff.py # tmp fix
+cp CmsHi/JetAnalysis/data/JEC_HI_PFTower_413patch2_2011_v3.db UserCode/ASYoon/FirstHi2011/PbPbSpectraAna/test
 
 # tools
 cvs co UserCode/ASYoon/PbPbTrackingTools/VertexConstraintProducer
@@ -29,13 +37,8 @@ cvs co UserCode/ASYoon/PbPbTrackingTools/HiCaloCompatibleTrackSelector
 #cvs co UserCode/edwenger/Skims/prod/condor # in case condor job scripts needed
 
 # needed for Matt's iterative tracking
-cvs co -d       MNguyen/iterTracking UserCode/MNguyen/iterTracking
-#cvs co -d       CmsHi/JetAnalysis UserCode/CmsHi/JetAnalysis
-#rm CmsHi/JetAnalysis/python/EGammaAnalyzers_cff.py # tmp fix
-#rm CmsHi/JetAnalysis/python/ExtraEGammaReco_cff.py # tmp fix
-#rm CmsHi/JetAnalysis/python/EventSelection_cff.py # tmp fix
+cvs co -d  MNguyen/iterTracking UserCode/MNguyen/iterTracking
 
-# needed for dijetana
 
 # setting and cleanup
 mv UserCode/edwenger .
