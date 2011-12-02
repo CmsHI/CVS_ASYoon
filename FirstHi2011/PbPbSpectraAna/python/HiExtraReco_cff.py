c7adfeb43f6cf463f0708daecd44a9190a0c6557
sungho <>
2011-12-02 22:08:39 +0000

    fix to use CaloTower

--- FirstHi2011/PbPbSpectraAna/python/HiExtraReco_cff.py
+++ FirstHi2011/PbPbSpectraAna/python/HiExtraReco_cff.py
@@ -9,17 +9,17 @@ hiExtraTrack = cms.Sequence(hiGoodTightTracks)   # highPurity from hiSelectedTra
 
 
 ## hiOptimalTightTracks
-hiPreCaloCompTracks = hiCaloCompatibleTracks.clone(srcTracks = cms.InputTag("hiSelectedTracks"),
-                                                   usePFCandMatching = cms.untracked.bool(True))
+hiPreCaloCompTracks = hiCaloCompatibleTracks.clone(srcTracks = cms.InputTag("hiGeneralTracks"),
+                                                   usePFCandMatching = cms.untracked.bool(False), # use CaloTower
+                                                   trkPtMin = cms.untracked.double(0.0))
 
-hiPreOptimalTightTracks = hiTracksWithLooseQuality.clone(src = cms.InputTag("hiGeneralTracks"),
+hiPreOptimalTightTracks = hiTracksWithLooseQuality.clone(src = cms.InputTag("hiPreCaloCompTracks"),
                           keepAllTracks = cms.bool(False),
                           chi2n_no1Dmod_par = cms.double(0.15))
 
 hiOptimalTightTracks = cms.EDFilter("TrackSelector",
                        src = cms.InputTag("hiPreOptimalTightTracks"),
-                       cut = cms.string('quality("highPurity") || quality("tight")'))
-
+                       cut = cms.string('quality("tight") || quality("highPurity")'))
 
 hiOptTrack = cms.Sequence(hiPreCaloCompTracks*
                           hiPreOptimalTightTracks*
