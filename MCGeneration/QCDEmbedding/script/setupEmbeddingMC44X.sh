77739a12ac66da97d7c80c21f8ac24050ec48a17
sungho <>
2011-12-05 23:27:59 +0000

    first time check in

---
+++ MCGeneration/QCDEmbedding/script/setupEmbeddingMC44X.sh
@@ -0,0 +1,13 @@
+
+cvs co UserCode/yinglu/MC_Production/Dijet
+cvs co UserCode/davidlw/MCProduction/python
+
+## GenProduction
+cvs co Configuration/GenProduction/python/customiseCaloDigisNZS.py
+cp UserCode/davidlw/MCProduction/python/*.py Configuration/GenProduction/python/
+
+## Generator 
+cvs co Configuration/Generator/python/PyquenDefaultSettings_cff.py 
+cvs co Configuration/Generator/python/PythiaUEZ2Settings_cfi.py 
+cvs co Configuration/Generator/python/PythiaUESettings_cfi.py
+cp UserCode/davidlw/MCProduction/python/PyquenTuneZ2Settings*.* Configuration/Generator/python/
